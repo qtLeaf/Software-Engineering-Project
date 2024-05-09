3f0824ecde13934a6efcd21434193a598eed4086
Ermes <77642795+qtLeaf@users.noreply.github.com>
2024-05-09 11:06:59 +0200

    Delete WordAutoma directory

--- WordAutoma/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module it.univr.wordautoma {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens it.univr.wordautoma to javafx.fxml;
-    exports it.univr.wordautoma;
-}
